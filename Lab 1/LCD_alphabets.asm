936c7aede190ede5dbb3efedaf94f71f2ff28a99
Muchen He <mansurhe@outlook.com>
2017-01-09 23:16:11 -0800

    new file

---
+++ Lab 1/LCD_alphabets.asm
@@ -0,0 +1,229 @@
+; LCD_name.asm: lab using LCD
+$NOLIST
+$MODLP52
+$LIST
+
+org 0000H
+    ljmp    setup
+
+org 001BH
+    ljmp    181BH
+
+; defines pins for LCD
+LCD_RS  equ P1.2
+LCD_RW  equ P1.3
+LCD_E   equ P1.4
+LCD_D4  equ P3.2
+LCD_D5  equ P3.3
+LCD_D6  equ P3.4
+LCD_D7  equ P3.5
+
+; defines for the other pins
+LED_RED equ P3.7
+
+;---------------------------------;
+; Wait 40 microseconds            ;
+;---------------------------------;
+sleep40us:
+    push    AR0
+    mov     R0, #177
+L0: nop
+    nop
+    djnz    R0, L0
+    pop     AR0
+    ret
+
+;---------------------------------;
+; Wait 'R0' milliseconds          ;
+;---------------------------------;
+sleep:
+    push    AR1
+    push    AR2
+L1: mov     R1, #45
+L2: mov     R2, #164
+L3: djnz    R2, L3
+    djnz    R1, L2
+    djnz    R0, L1
+    pop     AR2
+    pop     AR1
+    ret
+
+;---------------------------------;
+; Toggles the LCD's 'E' pin       ;
+;---------------------------------;
+LCD_pulse:
+    setb    LCD_E
+    lcall   sleep40us
+    clr     LCD_E
+    ret
+
+
+;---------------------------------;
+; Writes data to LCD              ;
+;---------------------------------;
+LCD_writeData:
+    setb    LCD_RS
+    ljmp    LCD_byte
+
+;---------------------------------;
+; Writes command to LCD           ;
+;---------------------------------;
+LCD_writeCommand:
+    clr     LCD_RS
+    ljmp    LCD_byte
+
+
+;---------------------------------;
+; Writes acc to LCD in 4-bit mode ;
+;---------------------------------;
+LCD_byte:
+    ; Write high 4 bits first
+    mov     c,          ACC.7
+    mov     LCD_D7,     c
+    mov     c,          ACC.6
+    mov     LCD_D6,     c
+    mov     c,          ACC.5
+    mov     LCD_D5,     c
+    mov     c,          ACC.4
+    mov     LCD_D4,     c
+    lcall   LCD_pulse
+
+    ; Write low 4 bits next
+    mov     c,          ACC.3
+    mov     LCD_D7,     c
+    mov     c,          ACC.2
+    mov     LCD_D6,     c
+    mov     c,          ACC.1
+    mov     LCD_D5,     c
+    mov     c,          ACC.0
+    mov     LCD_D4,     c
+    lcall   LCD_pulse
+    ret
+
+;---------------------------------;
+; Configure LCD in 4-bit mode     ;
+;---------------------------------;
+LCD_configure_4bit:
+    ; Resting state of LCD's enable is zero
+    clr     LCD_E
+
+    ; We are only writing to the LCD in this program
+    clr     LCD_RW
+
+    ; After power on, wait for LCD start up time before initializing
+    ; NOTE: the preprogrammed power-on delay of 16 ms on the AT89LP52
+    ; seems to be enough.  That is why these two lines are commented out.
+    ; Also, commenting these two lines improves simulation time in Multisim.
+    ; mov R2, #40
+    ; lcall WaitmilliSec
+
+    ; First make sure the LCD is in 8-bit mode and then change to 4-bit mode
+    mov     a,  #0x33
+    lcall   LCD_writeCommand
+    mov     a,  #0x33
+    lcall   LCD_writeCommand
+    mov     a,  #0x32 ; change to 4-bit mode
+    lcall   LCD_writeCommand
+
+    ; Configure the LCD
+    mov     a,  #0x28
+    lcall   LCD_writeCommand
+    mov     a,  #0x0c
+    lcall   LCD_writeCommand
+    mov     a,  #0x01 ;  Clear screen command (takes some time)
+    lcall   LCD_writeCommand
+
+    ;Wait for clear screen command to finish. Usually takes 1.52ms.
+    mov     R0, #2
+    lcall   sleep
+    ret
+
+
+;---------------------------------;
+; Clear LCD                       ;
+;---------------------------------;
+LCD_clear:
+    push    AR0
+    mov     R0, #0x80
+L6: mov     a,  R0
+    lcall   LCD_writeCommand
+    mov     a,  #32
+    lcall   LCD_writeData
+    cjne    R0, #0x8F,  L7
+
+    mov     R0, #0xC0
+L8: mov     a,  R0
+    lcall   LCD_writeCommand
+    mov     a,  #32
+    lcall   LCD_writeData
+    cjne    R0, #0xCF,  L9
+    pop     AR0
+    ret
+
+L9: inc     R0
+    sjmp    L8
+
+L7: inc     R0
+    sjmp    L6
+
+;---------------------------------;
+; Main functions                  ;
+; SETUP function runs onces       ;
+;---------------------------------;
+setup:
+    mov     SP,     #7FH
+    mov     PMOD,   #0
+
+    ; configure LCD
+    lcall   LCD_configure_4bit
+
+    ; setup for animation
+    mov     R1,     #0x80
+    mov     R2,     #'A'
+    mov     R3,     #0xCF
+    mov     R4,     #'0'
+
+;---------------------------------;
+; Main functions                  ;
+; LOOP function runs forever      ;
+;---------------------------------;
+loop:
+    ; clear LCD
+    lcall   LCD_clear
+
+    ; write letter to LCD
+    mov     a,      R1
+    lcall   LCD_writeCommand
+    mov     a,      R2
+    lcall   LCD_writeData
+    mov     a,      R3
+    lcall   LCD_writeCommand
+    mov     a,      R4
+    lcall   LCD_writeData
+
+    ; increment position of cursor
+    ; check if it's at the end
+    cjne    R1,     #0x8F,  L4
+    mov     R1,     #0x7F
+L4: inc     R1
+    cjne    R2,     #'Z',   L5
+    mov     R2,     #'@'
+L5: inc     R2
+
+    cjne    R3,     #0xC0,  LA
+    mov     R3,     #0xD0
+LA: dec     R3
+    cjne    R4,     #'9',   LB
+    mov     R4,     #'/'
+LB: inc     R4
+
+	; blink the LED
+    cpl     LED_RED
+
+    ; sleep the program
+    mov     R0,     #80
+    lcall   sleep
+    sjmp    loop
+
+; end of program
+END
