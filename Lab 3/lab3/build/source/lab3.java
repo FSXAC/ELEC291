b7f90afb6433c4545fedb8096a6b0baf45c7be8c
Muchen He <mansurhe@outlook.com>
2017-01-25 01:27:52 -0800

    added processing project for serial input

--- Lab 3/lab3/build/source/lab3.java
+++ Lab 3/lab3/build/source/lab3.java
@@ -18,9 +18,21 @@ public class lab3 extends PApplet {
 
 
 
-Serial  SPI_at89;
+Serial  at89;
 int     ADC_value;
 int     BAUD_RATE = 115200;
+
+int   adc_value;
+float adc_temperature;
+float adc_voltage;
+float clc_temperature;
+
+int x = 0;
+float y, y_prev, y_tgt;
+float y2, y2_prev, y2_tgt;
+
+float v_offset     = 0;
+float v_offset_tgt = 0;
 
 public void setup() {
     
@@ -29,71 +41,104 @@ public void setup() {
     printArray(Serial.list());
 
     // initialze serial
-    SPI_at89 = new Serial(this, Serial.list()[0], BAUD_RATE);
+    at89 = new Serial(this, Serial.list()[0], BAUD_RATE);
     background(0);
+
+    // get rid of garbage values
+    if (getStringSPI(611) < 1000) {
+        getStringSPI(611);
+    }
 }
 
-
-int     ADC_number;
-String  SPI_read;
-float   V_out;
-float   temperature;
-
-int x = 0;
-float y, y_prev, y_tgt;
-
 public void draw() {
-    if (SPI_at89.available() > 0) {
-        SPI_read = SPI_at89.readString();
-        println(SPI_read);
+    if (at89.available() > 0) {
 
-        if (SPI_read.length() > 3 && SPI_read != "\r\n") {
-            SPI_read = SPI_read.substring(0, SPI_read.length()-2);
-            SPI_read = SPI_read.replaceFirst("^0+(?!$)", "");
+        adc_value       = getStringSPI(adc_value);
+        adc_temperature = getStringSPI(PApplet.parseInt(adc_temperature * 100)) / 100;
 
-            try {
-                ADC_number = Integer.parseInt(SPI_read);
-            } catch (NumberFormatException e) {
-                println(e);
-            }
+        println(str(adc_value) + '\t' + str(adc_temperature));
 
-            V_out       = ADC_number / 1023.0f * 5;
-            temperature = (V_out - 2.73f) * 100;
+        adc_voltage = adc_value / 1023.0f * 5;
+        clc_temperature = (adc_voltage - 2.73f) * 100;
 
-            // graphic
-            if (temperature < -40) {
-                temperature = -40;
-            }
-            y_prev = y;
-            y_tgt  = map(temperature, -50, 120, height, 0);
-
-
-            fill(0, 5);
-            rect(0, 50, width, height-50);
-            fill(0);
-            rect(0, 0, width, 50);
-            fill(20);
-            rect(width - 80, 0, 80, height);
-
-            y = lerp(y, y_tgt, 0.8f);
-            stroke(
-                map(y, 0.3f * height, 0.7f * height, 255, 0),
-                (1 - sq(map(y, 0, height, -1, 1))) * 255,
-                map(y, 0.3f * height, 0.7f * height, 0, 255)
-                );
-            strokeWeight(3);
-            line(x, y, x-5, y_prev);
-            if (x >= width - 80) {
-                x = 0;
-            } else {
-                x+= 5;
-            }
-            noStroke();
-
-            fill(255);
-            text(String.format("%.2f", temperature) + " C", width - 75, y);
-            text(String.format("%.4f", V_out) + " V", width - 75, y + 12);
+        // bound
+        if (clc_temperature < -40) {
+            clc_temperature = -40;
         }
+
+        // fix adc temperature to signed
+
+
+        // for graphics
+        // rectangles
+        fill(0, 5);
+        rect(0, 0, width, height);
+        fill(20);
+        rect(width - 80, 0, 80, height);
+
+        // lerp
+        y_prev   = y;
+        y_tgt    = map(clc_temperature, 0, 40, height - 50, 50);
+        y2_prev  = y2;
+        y2_tgt   = map(adc_temperature, 0, 40, height - 50, 50);
+        y        = lerp(y, y_tgt, 0.7f);
+        y2       = lerp(y2, y2_tgt, 0.7f);
+        v_offset = lerp(v_offset, v_offset_tgt, 0.5f);
+
+        stroke(
+            map(y, 0.3f * (height - 50), 0.7f * (height - 50), 255, 0),
+            sq(map(y, 0, height, -1, 1)) * 255,
+            map(y, 0.3f * (height - 50), 0.7f * (height - 50), 0, 255)
+            );
+        line(x, y2 + v_offset, x-5, y2_prev + v_offset);
+        strokeWeight(3);
+        stroke(
+            map(y, 0.3f * (height - 50), 0.7f * (height - 50), 255, 0),
+            (1 - sq(map(y, 0, height, -1, 1))) * 255,
+            map(y, 0.3f * (height - 50), 0.7f * (height - 50), 0, 255)
+            );
+        line(x, y + v_offset, x-5, y_prev + v_offset);
+
+        if (x >= width - 80) {
+            x = 0;
+        } else {
+            x+= 5;
+        }
+        noStroke();
+
+        fill(255);
+        text(String.format("%.2f", clc_temperature) + " C", width - 75, y + v_offset);
+        text(String.format("%.4f", adc_voltage) + " V", width - 75, y + 12 + v_offset);
+    }
+}
+
+public void mouseWheel(MouseEvent event) {
+    v_offset_tgt += 20 * event.getCount();
+}
+
+public int getStringSPI(int default_val) {
+    char readChar        = 'x';
+    String readStr       = "";
+    Boolean isFirstDigit = true;
+
+    // loop through the first number (ADC number)
+    while (readChar != '\r') {
+        readChar = at89.readChar();
+        if (isFirstDigit == true && readChar == '0') {
+        } if (readChar != '\r') {
+            readStr = readStr + readChar;
+        }
+        isFirstDigit = false;
+    }
+
+    // read and ignore \n
+    at89.readChar();
+
+    // return
+    try {
+        return Integer.parseInt(readStr);
+    } catch (NumberFormatException e) {
+        return default_val;
     }
 }
   public void settings() {  size(500, 300); }
