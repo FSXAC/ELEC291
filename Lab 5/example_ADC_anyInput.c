8e53518b0a928fc6675a9b38037efc304dd3e31f
Muchen He <mansurhe@outlook.com>
2017-03-07 21:19:49 -0800

    updated c51 external function

--- Lab 5/example_ADC_anyInput.c
+++ Lab 5/example_ADC_anyInput.c
@@ -13,60 +13,54 @@
 // measured value of VDD in volts
 #define VDD 3.325
 
-char _c51_external_startup (void) {
-    PCA0MD &= (~0x40);    // DISABLE WDT: clear Watchdog Enable bit
+char _c51_external_startup (void)
+{
+    PCA0MD &= (~0x40) ;    // DISABLE WDT: clear Watchdog Enable bit
+    VDM0CN  = 0x80; // enable VDD monitor
+    RSTSRC  = 0x02|0x04; // Enable reset on missing clock detector and VDD
 
     // CLKSEL&=0b_1111_1000; // Not needed because CLKSEL==0 after reset
     #if (SYSCLK == 12000000L)
         //CLKSEL|=0b_0000_0000;  // SYSCLK derived from the Internal High-Frequency Oscillator / 4
     #elif (SYSCLK == 24000000L)
-        CLKSEL |= 0b_0000_0010; // SYSCLK derived from the Internal High-Frequency Oscillator / 2.
+        CLKSEL|=0b_0000_0010; // SYSCLK derived from the Internal High-Frequency Oscillator / 2.
     #elif (SYSCLK == 48000000L)
-        CLKSEL |= 0b_0000_0011; // SYSCLK derived from the Internal High-Frequency Oscillator / 1.
+        CLKSEL|=0b_0000_0011; // SYSCLK derived from the Internal High-Frequency Oscillator / 1.
     #else
         #error SYSCLK must be either 12000000L, 24000000L, or 48000000L
     #endif
+
     OSCICN |= 0x03; // Configure internal oscillator for its maximum frequency
 
-    // Configure P2.0 as Analog Input
-    P2MDIN &= 0b_1111_1110; // P2.0 only
-    P2SKIP |= 0b_0000_0001; // Skip Crossbar decoding for this pin
-
-    // Init ADC multiplexer to read the voltage between P2.0 and ground.
-    // IMPORTANT: check section 6.5 in datasheet.  The constants for
-    // each pin are available in "c8051f38x.h" both for the 32 and 48
-    // pin packages.
-    AMX0P = LQFP32_MUX_P2_0; // Select positive input from P0.0
-    AMX0N = LQFP32_MUX_GND;  // GND is negative input (Single-ended Mode)
-
-    VDM0CN   = 0x80;       // enable VDD monitor
-    RSTSRC   = 0x02|0x04;  // Enable reset on missing clock detector and VDD
-    P0MDOUT |= 0x11;     // Enable Uart TX as push-pull output, P0.0 as output
-    XBR0     = 0x01;         // Enable UART on P0.4(TX) and P0.5(RX)
-    XBR1     = 0x40;         // Enable crossbar and weak pull-ups
-
+    // Configure UART0
+    SCON0 = 0x10;
     #if (SYSCLK/BAUDRATE/2L/256L < 1)
-        TH1    = 0x10000-((SYSCLK/BAUDRATE)/2L);
-        CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
-        CKCON |= 0x08;
+    	TH1 = 0x10000-((SYSCLK/BAUDRATE)/2L);
+    	CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
+    	CKCON |=  0x08;
     #elif (SYSCLK/BAUDRATE/2L/256L < 4)
-        TH1    = 0x10000-(SYSCLK/BAUDRATE/2L/4L);
-        CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 01
-        CKCON |= 0x01;
+    	TH1 = 0x10000-(SYSCLK/BAUDRATE/2L/4L);
+    	CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 01
+    	CKCON |=  0x01;
     #elif (SYSCLK/BAUDRATE/2L/256L < 12)
-        TH1    = 0x10000-(SYSCLK/BAUDRATE/2L/12L);
-        CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 00
+    	TH1 = 0x10000-(SYSCLK/BAUDRATE/2L/12L);
+    	CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 00
     #else
-        TH1    = 0x10000-(SYSCLK/BAUDRATE/2/48);
-        CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 10
-        CKCON |= 0x02;
+    	TH1 = 0x10000-(SYSCLK/BAUDRATE/2/48);
+    	CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 10
+    	CKCON |=  0x02;
     #endif
-
-    TL1   = TH1;     // Init timer 1
-    TMOD &= 0x0f;  // TMOD: timer 1 in 8-bit autoreload
+    
+    TL1   = TH1;      // Init Timer1
+    TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit autoreload
     TMOD |= 0x20;
-    TR1   = 1;       // Start timer1
-    SCON  = 0x52;
+    TR1   = 1; // START Timer1
+    TI    = 1;  // Indicate TX0 ready
+
+    // Configure the pins used for motor control and communication
+    P0MDOUT |= 0x01;  // set P0.0 and P0.4 as push-pull outputs
+    XBR0 = 0x01;      // Enable UART0 on P0.4(TX0) and P0.5(RX0)
+    XBR1 = 0x40;      // enable crossbar
 
     return 0;
 }
@@ -74,25 +68,25 @@ char _c51_external_startup (void) {
 // Use timer 3 to delay <us> micro-seconds
 void delayUs(unsigned char us) {
     unsigned char i;
-	CKCON  |= 0b_0100_0000;
-	TMR3RL  = (-(SYSCLK)/1000000L);
-	TMR3    = TMR3RL;
-	TMR3CN  = 0x04;
+    CKCON  |= 0b_0100_0000;
+    TMR3RL  = (-(SYSCLK)/1000000L);
+    TMR3    = TMR3RL;
+    TMR3CN  = 0x04;
 
     // count <us> overflows
-	for (i = 0; i < us; i++) {
-		while (!(TMR3CN & 0x80));
-		TMR3CN &= ~(0x80);
-	}
-	TMR3CN = 0;
+    for (i = 0; i < us; i++) {
+        while (!(TMR3CN & 0x80));
+        TMR3CN &= ~(0x80);
+    }
+    TMR3CN = 0;
 }
 
 // delays <ms> microseconds
 void delay(unsigned int ms) {
-	unsigned int j;
-	unsigned char k;
-	for (j = 0; j < ms; j++)
-		for (k = 0; k < 4; k++) delayUs(250);
+    unsigned int j;
+    unsigned char k;
+    for (j = 0; j < ms; j++)
+        for (k = 0; k < 4; k++) delayUs(250);
 }
 
 // Init ADC
